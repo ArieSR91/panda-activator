543d344231e54c96305f9496e243848071b2e7c3
ArieSR91 <syahri.ramadhan2022@gmail.com>
2024-12-14 23:14:26 +0800

    by Arie-SR91

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 if [ ! -d /data/data/com.panda.mouse ]; then
 	xdg-open .github/pandamousepro.apk
+	sleep 3
+	echo -e "Panda Mouse Pro already installed\nNow you can run: bash activate.sh"
 else
 	echo -e "Panda Mouse Pro already installed\nNow you can run: bash activate.sh"
 fi
